961fd524418df7965ac597ae552bedde6a5247b2
kilianruiz <451093.joan23@fje.edu>
2024-11-05 17:07:50 +0100

    Inicio del Aplicativo

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/styles.css"> 
+</head>
+<body>
+    <div>
+        <div>
+            <img src="" alt="">  <!-- logo de la pagina -->
+        </div>
+        <div>
+            <form action="" method="POST">
+                <input type="text" name="usuario" placeholder="Nombre de usuario...">
+                <input type="password" name="password" placeholder="Contraseña...">
+                <button>ENTRAR</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
