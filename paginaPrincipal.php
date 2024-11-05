43092f6e26b45c288ae180689edd09304fdc76eb
kilianruiz <451093.joan23@fje.edu>
2024-11-05 20:51:05 +0100

    Variables de session

--- paginaPrincipal.php
+++ paginaPrincipal.php
@@ -1,7 +1,14 @@
 <!-- Eleccion de los diferentes salones -->
 <?php session_start(); 
-include_once('../conexion/conexion.php');
+include('./conexion/conexion.php');
+
+if (!isset($_SESSION['usuario'])) {
+    echo "<h6>Por favor, inicie sesión.</h6>";
+    exit;
+}
+
 ?>
+
 
 <form action="./cerrarSesion/logout.php" method="POST">
     <button>Cerrar sesion</button>
