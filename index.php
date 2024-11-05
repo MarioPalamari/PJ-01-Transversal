c593646de07ba7730c0bdeecac2b6789d8a6a8e3
kilianruiz <451093.joan23@fje.edu>
2024-11-05 22:06:57 +0100

    Actualizacion del Login

--- index.php
+++ index.php
@@ -4,29 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./css/styles.css"> 
+    <link rel="stylesheet" href="./css/styles.css">
 </head>
 <body>
     <div>
         <div>
-            <img src="./img/logo.webp" alt="">  <!-- logo de la pagina -->
+            <img src="./img/logo.webp" alt="Logo de la página">
         </div>
         <div>
             <form action="./procLogin/validacionLogin.php" method="POST">
-                <input type="text" name="usuario" placeholder="Nombre de usuario..." onblur="validaNombre()">
-                <input type="password" name="password" placeholder="Contraseña..." onblur="validaContraseña()">
-                <button>ENTRAR</button>
+                <input type="text" name="usuario" placeholder="Nombre de usuario...">
+                <input type="password" name="password" placeholder="Contraseña...">
+                <button type="submit">ENTRAR</button>
                 <?php
-                    if (isset($_GET['error'])) {
-                        $error = $_GET['error'];
-                        echo "<p style='color: red'>Usuario o contraseña incorrectos </p>";
-                    }
+                // Mostrar mensaje de error si la autenticación falla
+                if (isset($_GET['error']) && $_GET['error'] == 1) {
+                    echo "<p style='color: red'>Usuario o contraseña incorrectos</p>";
+                }
                 ?>
             </form>
         </div>
     </div>
-
-
-<script src="./validaciones/validaciones.js"></script>
+    <script src="./validaciones/validaciones.js"></script>
 </body>
 </html>
