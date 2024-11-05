1a6ae5e65289c0e237c4b2c570a50bcb7fadf36f
kilianruiz <451093.joan23@fje.edu>
2024-11-05 17:11:23 +0100

    Inicio de la base de datos

---
+++ BBDD/bbdd.sql
@@ -0,0 +1,10 @@
+CREATE DATABSASE bd_restaurante;
+
+use bd_restaurante;
+
+create table tbl_camareros {
+    id_camarero int auto_increment not null,
+    nombre_camarero varchar(50) not null,
+    apellido_camarero varchar(50) not null;
+}
+
