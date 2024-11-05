2a45a1c76390708282a9b768e44eb5713123084b
kilianruiz <451093.joan23@fje.edu>
2024-11-05 17:30:39 +0100

    Actualizacion de la BBDD

--- BBDD/bbdd.sql
+++ BBDD/bbdd.sql
@@ -5,6 +5,7 @@ use bd_restaurante;
 create table tbl_camareros {
     id_camarero int auto_increment not null,
     nombre_camarero varchar(50) not null,
-    apellido_camarero varchar(50) not null;
+    apellido_camarero varchar(50) not null,
+    password_camarero varchar(255) not null;
 }
 
